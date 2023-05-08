8bc70f8bc2facf07aa4796300b2bcff9b0422e7b
shokHorizon <kostan0008@gmail.com>
2023-05-08 17:14:35 +0300

    Prichesal

--- internals/driver/command/command.go
+++ internals/driver/command/command.go
@@ -6,6 +6,33 @@ import (
 	"io"
 )
 
+const (
+	CMD_GET_SCALE_PAR byte = 0x75
+	CMD_ACK_SCALE_PAR byte = 0x76
+	CMD_ERROR         byte = 0x28
+	CMD_GET_MASSA     byte = 0x23
+	CMD_ACK_MASSA     byte = 0x24
+	CMD_SET_TARE      byte = 0xA3
+	CMD_ACK_SET_TARE  byte = 0x12
+	CMD_NACK_TARE     byte = 0x15
+	CMD_SET_ZERO      byte = 0x72
+	CMD_ACK_SET       byte = 0x27
+	CMD_GET_NAME      byte = 0x20
+	CMD_ACK_NAME      byte = 0x21
+	CMD_SET_NAME      byte = 0x22
+	CMD_GET_ETHERNET  byte = 0x2D
+	CMD_ACK_ETHERNET  byte = 0x2E
+	CMD_SET_ETHERNET  byte = 0x39
+	CMD_GET_WIFI_IP   byte = 0x33
+	CMD_ACK_WIFI_IP   byte = 0x34
+	CMD_SET_WIFI_IP   byte = 0x31
+	CMD_GET_WIFI_SSID byte = 0x3A
+	CMD_SET_WIFI_SSID byte = 0x3C
+	CMD_NACK          byte = 0x3F
+)
+
+var HEADERS = [3]byte{0xF8, 0x55, 0xCE}
+
 type Head struct {
 	Headers [3]byte
 	Len     int16
@@ -133,30 +160,3 @@ func GetErrorMesage(code byte) string {
 	}
 	return "Такой ошибки даже в доке нет..."
 }
-
-var HEADERS = [3]byte{0xF8, 0x55, 0xCE}
-
-const (
-	CMD_GET_SCALE_PAR byte = 0x75
-	CMD_ACK_SCALE_PAR byte = 0x76
-	CMD_ERROR         byte = 0x28
-	CMD_GET_MASSA     byte = 0x23
-	CMD_ACK_MASSA     byte = 0x24
-	CMD_SET_TARE      byte = 0xA3
-	CMD_ACK_SET_TARE  byte = 0x12
-	CMD_NACK_TARE     byte = 0x15
-	CMD_SET_ZERO      byte = 0x72
-	CMD_ACK_SET       byte = 0x27
-	CMD_GET_NAME      byte = 0x20
-	CMD_ACK_NAME      byte = 0x21
-	CMD_SET_NAME      byte = 0x22
-	CMD_GET_ETHERNET  byte = 0x2D
-	CMD_ACK_ETHERNET  byte = 0x2E
-	CMD_SET_ETHERNET  byte = 0x39
-	CMD_GET_WIFI_IP   byte = 0x33
-	CMD_ACK_WIFI_IP   byte = 0x34
-	CMD_SET_WIFI_IP   byte = 0x31
-	CMD_GET_WIFI_SSID byte = 0x3A
-	CMD_SET_WIFI_SSID byte = 0x3C
-	CMD_NACK          byte = 0x3F
-)
